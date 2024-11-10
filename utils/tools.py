5ce652b0bb765790f0adf6531e413f772d43a79d
Jonah May <119529402+JonahMMay@users.noreply.github.com>
2024-11-10 12:16:31 -0600

    Update tools.py

--- utils/tools.py
+++ utils/tools.py
@@ -2,14 +2,10 @@ import torch
 from .text.cleaners import Cleaner
 from .text.tokenizer import Tokenizer
 
-def prepare_text(text: str, models_dir: str) -> torch.Tensor:
+def prepare_text(text: str, models_dir: str, device: torch.device) -> torch.Tensor:
     if not text[-1] in '.?!':
         text += '.'
     cleaner = Cleaner('english_cleaners', True, 'en-us', models_dir=models_dir)
     tokenizer = Tokenizer()
     tokens = tokenizer(cleaner(text))
-    
-    # Detect device
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    
     return torch.as_tensor(tokens, dtype=torch.long, device=device).unsqueeze(0)
