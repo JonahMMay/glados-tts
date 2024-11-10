2321b9d5bc52c6cbc44459080e5aae25ff67e6b5
Jonah May <119529402+JonahMMay@users.noreply.github.com>
2024-11-10 11:19:45 -0600

    Update glados.py

--- glados.py
+++ glados.py
@@ -28,11 +28,11 @@ class tts_runner:
     def __init__(self, use_p1: bool=False, log: bool=False, models_dir: str='models'):
         self.log = log
         self.models_dir = models_dir
-        if use_p1:
-            emb_path = os.path.join(self.models_dir, 'emb', 'glados_p1.pt')
-        else:
-            emb_path = os.path.join(self.models_dir, 'emb', 'glados_p2.pt')
-        self.emb = torch.load(emb_path)
+        emb_filename = 'glados_p1.pt' if use_p1 else 'glados_p2.pt
+        emb_path = self.models_dir / 'emb' / emb_filename
+        if not emb_path.is_file():
+            raise FileNotFoundError(f"Embedding model not found at {emb_path}")
+        self.emb = torch.load(str(emb_path))
         # Select the device
         if torch.cuda.is_available():
             self.device = 'cuda'
